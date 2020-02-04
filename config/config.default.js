52e869773bfd38a0514661cf132d95ff12506049
doramart <doramart@qq.com>
2020-02-04 12:30:06 +0800

    update egg-dora-announce

--- config/config.default.js
+++ config/config.default.js
@@ -17,7 +17,7 @@ exports.doraAnnounce = {
     isindex: 0, // 是否需要前台访问，1：需要，0：不需要,入口地址:'/ext/devteam/index/index'
     version: pkgInfo.version, // 版本号
     iconName: 'icon_inform_fill', // 主菜单图标名称
-    adminUrl: 'https://cdn.html-js.cn/cms/plugins/static/admin/announce/js/app.js',
+    adminUrl: '/announce/js/app.js',
     adminApi: [{
         url: 'systemAnnounce/getList',
         method: 'get',
@@ -35,12 +35,11 @@ exports.doraAnnounce = {
         details: '删除公告',
     }],
     fontApi: [],
-    
+
     initData: '', // 初始化数据脚本
     pluginsConfig: ` 
     exports.doraAnnounce = {\n
-        enable: true,\n
-        \n
+        enable: true,\n        package: 'egg-dora-announce',
     };\n
     `, // 插入到 plugins.js 中的配置
     defaultConfig: `
